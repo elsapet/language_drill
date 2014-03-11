2b7fcb6656d46bbf9a5e9d1a600ef8392d269d7a
elsapet <ebraae@gmail.com>
2014-03-11 21:32:11 +0200

    Clean up

--- language_drill.py
+++ language_drill.py
@@ -2,25 +2,23 @@ import csv
 
 vocab = {}
 
-with open('german.csv', newline='') as csvfile:
+file_name = input("Please enter file name e.g. 'vocab.csv'\n")
+
+with open(file_name, newline='') as csvfile:
     vocab_file = csv.reader(csvfile, delimiter=';', quotechar='|')
     for row in vocab_file:
         vocab[row[0]] = row[1]
 
+print("Ready? Here we go!\n")
+
 for word in vocab:
     
-    if len(word) < 5: tab = "\t\t\t"
-    elif len(word) > 15: tab = "\t"
-    else : tab = "\t\t"
-    
-    print(word,tab,end="")
-    given_word = input()
+    given_word = input(word+"\t\t")
     while vocab[word] != given_word:
         print("Try again or type 'help'")
-        print(word,tab,end="")
-        given_word = input()
+        given_word = input(word+"\t\t")
         if given_word.lower() == "help":
             print("'"+word+"' is '"+vocab[word]+"'")
             break
 
-print("You've drilled",len(vocab),"words! Good job!")
+print("\nYou've drilled",len(vocab),"words! Good job!")
